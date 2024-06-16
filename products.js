427849ac9fb7399035d548a6d03a53708af9b596
kuldeep <kuldeepgautam06082001@gmail.com>
2024-06-16 13:18:43 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id : 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price : 999,
+        Image : "https://res.cloudinary.com/dryjgkyob/image/upload/v1718504724/imgForAPI/ubk8d0rsfbxl586mabtl.png"
+    },
+    {
+        id : 2,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price : 999,
+        Image : "https://res.cloudinary.com/dryjgkyob/image/upload/v1718504389/imgForAPI/ote1l9oodbdybncywpmo.png"
+    },
+    {
+        id : 3,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price : 999,
+        Image : "https://res.cloudinary.com/dryjgkyob/image/upload/v1718504379/imgForAPI/ftms9trhr13d5yva9kto.png"
+    },
+    {
+        id : 4,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price : 999,
+        Image : "https://res.cloudinary.com/dryjgkyob/image/upload/v1718504379/imgForAPI/ftms9trhr13d5yva9kto.png"
+    },
+]
+
+module.exports = products;
